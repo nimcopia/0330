a3073f3d394de05fc0dc6385322f09a7bbe82567
nimcopia <nicofan71@gmail.com>
2023-03-14 16:15:10 +0900

    3.14

---
+++ src/main/java/com/study/domain/post/PostController.java
@@ -0,0 +1,27 @@
+package com.study.domain.post;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    // 게시글 작성 페이지
+     // 게시글 작성 페이지
+       // 게시글 작성 페이지
+    @GetMapping("/post/write.do")
+    public String openPostWrite(@RequestParam(value = "id", required = false) final Long id, Model model) {
+        if (id != null) {
+            PostResponse post = postService.findPostById(id);
+            model.addAttribute("post", post);
+        }
+        return "post/write";
+    }
+
+}
