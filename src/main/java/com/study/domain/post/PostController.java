96e008eacf6d6a4924ab1b462ea18dda36a47353
nimcopia <nicofan71@gmail.com>
2023-03-15 15:32:12 +0900

    3m15d

--- src/main/java/com/study/domain/post/PostController.java
+++ src/main/java/com/study/domain/post/PostController.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,9 +15,7 @@ public class PostController {
     private final PostService postService;
 
     // 게시글 작성 페이지
-     // 게시글 작성 페이지
-       // 게시글 작성 페이지
-    @GetMapping("/post/write.do")
+       @GetMapping("/post/write.do")
     public String openPostWrite(@RequestParam(value = "id", required = false) final Long id, Model model) {
         if (id != null) {
             PostResponse post = postService.findPostById(id);
@@ -24,4 +24,39 @@ public class PostController {
         return "post/write";
     }
 
+      // 신규 게시글 생성
+    @PostMapping("/post/save.do")
+    public String savePost(final PostRequest params) {
+        postService.savePost(params);
+        return "redirect:/post/list.do";
+    }
+
+    // 게시글 리스트 페이지
+    @GetMapping("/post/list.do")
+    public String openPostList(Model model) {
+        List<PostResponse> posts = postService.findAllPost();
+        model.addAttribute("posts", posts);
+        return "post/list";
+    }
+
+     // 게시글 상세 페이지
+    @GetMapping("/post/view.do")
+    public String openPostView(@RequestParam final Long id, Model model) {
+        PostResponse post = postService.findPostById(id);
+        model.addAttribute("post", post);
+        return "post/view";
+    }
+
+      @PostMapping("/post/update.do")
+    public String updatePost(final PostRequest params) {
+        postService.updatePost(params);
+        return "redirect:/post/list.do";
+    }
+
+    // 게시글 삭제
+    @PostMapping("/post/delete.do")
+    public String deletePost(@RequestParam final Long id) {
+        postService.deletePost(id);
+        return "redirect:/post/list.do";
+    }
 }
